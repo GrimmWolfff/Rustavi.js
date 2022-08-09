1e8ae78cf46d54ae0ad50aaf6005bfc67d376549
Fugazzi <sichinavailia@gmail.com>
2022-08-10 01:29:12 +0400

    1st

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import { createServer } from 'http';
+import fetch from 'node-fetch';
+class Rustavi {
+    async get(url, headers) {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: headers
+        });
+        return response;
+    }
+}
+
+const server = createServer(( req, res ) => {
+    if(req.url == '/' && req.method == 'GET') {
+        console.log('done boy');
+    }
+})
+server.listen(8000, () => 'Server Started');
+
+const rustavi = new Rustavi();
+rustavi.get('http://localhost:8000', { 'Content-Type':'application/json' });
